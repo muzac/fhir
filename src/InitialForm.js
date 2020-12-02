1b4eb7307679bad3da2a5a3da9016af20d0d65d4
emelmuzac <emel.muzac@eu.medical.canon>
2020-12-02 09:51:46 +0000

    Getting patient data

--- src/InitialForm.js
+++ src/InitialForm.js
@@ -4,7 +4,7 @@ import './InitialForm.css';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-
+import Parser from 'html-react-parser';
 
 class InitialForm extends Component {
     constructor(props) {
@@ -12,14 +12,16 @@ class InitialForm extends Component {
       this.state = {
         name: 'http://test.fhir.org/r3',
         columnDefs: [
-          {headerName: "ID", field: "id"},
+          {headerName: "ID", field: "id", width: 70},
           {headerName: "Name", field: "name"},
           {headerName: "Surname", field: "surname"},
           {headerName: "DOB", field: "dob"},
-          {headerName: "Gender", field: "gender"}
+          {headerName: "Gender", field: "gender",width: 50}
         ],
         rowData: [
-        ]
+        ],
+        showPatients: false,
+        showPatientDetailsId: -1
       };
     }
 
@@ -29,6 +31,8 @@ class InitialForm extends Component {
       });
     }
 
+    // http://test.fhir.org/r3/Patient?id=10&_format=json
+    // http://test.fhir.org/r3/Patient/10/?_format=json
     // https://vonk.fire.ly
     // http://test.fhir.org/r3
     handleClick() {
@@ -41,6 +45,7 @@ class InitialForm extends Component {
                 const error = (data && data.message) || response.statusText;
                 return Promise.reject(error);
             }
+            this.setState({ showPatients: true });
 
             let newRowData = [];
             data.entry.forEach(element => {
@@ -54,7 +59,6 @@ class InitialForm extends Component {
                 newRowData.push(newEntry);
             });
 
-            this.setState({ totalReactPackages: data.total })
             this.setState({rowData: newRowData})
         })
         .catch(error => {
@@ -63,21 +67,45 @@ class InitialForm extends Component {
         });
     }
 
+    handlePatientRowClick(rowIndex) {
+      console.log("row clicked", this.state.rowData[rowIndex].id);
+      fetch(this.state.name + '/Patient/' + this.state.rowData[rowIndex].id + '/' + '?_format=json&id=' )
+        .then(async response => {
+            const data = await response.json();
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response statusText
+                const error = (data && data.message) || response.statusText;
+                return Promise.reject(error);
+            }
+            this.setState({ showPatientDetailsId: data.text.div });
+        })
+        .catch(error => {
+          this.setState({ errorMessage: error.toString() });
+          console.error('There was an error!', error);
+      });
+    }
+
     render() {
       return (
         <div>
           <div>
-          <label class="label" htmlFor="name">FHIR Server </label>
-          <input class="input" type="text" id="name" value={this.state.name} onChange={this.changeText.bind(this)}/>
-          <button class="button" onClick={this.handleClick.bind(this)}>GO</button>
-          </div><div className="ag-theme-balham"
-				style={{
-					height: '500px',
-					width: '700px'
-				}}><AgGridReact
-					columnDefs={this.state.columnDefs}
-					rowData={this.state.rowData}>
-				</AgGridReact></div>
+            <label class="label" htmlFor="name">FHIR Server </label>
+            <input class="input" type="text" id="name" value={this.state.name} onChange={this.changeText.bind(this)}/>
+            <button class="button" onClick={this.handleClick.bind(this)}>GO</button>
+          </div>
+          { this.state.showPatients ? <div className="ag-theme-balham"
+            style={{
+              height: '500px',
+              width: '100%'
+            }}><AgGridReact
+              columnDefs={this.state.columnDefs}
+              rowData={this.state.rowData}
+              onRowClicked={(e) => this.handlePatientRowClick(e.rowIndex) }>
+            </AgGridReact>
+          </div> : null }
+          { this.state.showPatientDetailsId !== -1 ? <div class="details">{Parser(this.state.showPatientDetailsId)}
+          </div> : null }
         </div>
       );
     }
